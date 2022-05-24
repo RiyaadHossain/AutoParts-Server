9c4e1d43c60880db8173cb7cbd0dbe7d97480df9
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-24 12:56:29 +0600

    Order get and post api created

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { MongoClient, ServerApiVersion, ObjectId, LEGAL_TCP_SOCKET_OPTIONS } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  LEGAL_TCP_SOCKET_OPTIONS,
+} = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -22,6 +27,7 @@ async function run() {
   try {
     await client.connect();
     const partsCollection = client.db("AutoParts").collection("parts");
+    const ordersCollection = client.db("AutoParts").collection("orders");
 
     // GET API - AutoParts
     app.get("/parts", async (req, res) => {
@@ -30,13 +36,39 @@ async function run() {
     });
 
     // GET API - AutoPart
-    app.get("/part/:id", async(req, res) =>{
-      const id = req.params.id
-      const query = {_id: ObjectId(id)}
-      const result = await partsCollection.findOne(query)
+    app.get("/part/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // PUT API - Part
+    app.put("/part/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const part = req.body;
+      const updatedPart = {
+        $set: part,
+      };
+      const result = await partsCollection.updateOne(filter, updatedPart);
+      res.send(result);
+    });
+
+    // GET API - Order
+    app.get("/order", async(req, res) =>{
+      const email = req.headers.email
+      const filter = {email: email}
+      const result = await ordersCollection.find(filter).toArray()
       res.send(result)
     })
-    
+
+    // POST API - Order
+    app.post("/order", async (req, res) => {
+      const part = req.body;
+      const result = await ordersCollection.insertOne(part);
+      res.send(result);
+    });
   } finally {
     // Nothing Here
   }
