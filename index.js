5448c32fa561b396c2b5d58750ebe730d4f42496
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-24 16:22:21 +0600

    add reviews api added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
 } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,7 +29,10 @@ async function run() {
     await client.connect();
     const partsCollection = client.db("AutoParts").collection("parts");
     const ordersCollection = client.db("AutoParts").collection("orders");
+    const usersCollection = client.db("AutoParts").collection("users");
+    const reviewsCollection = client.db("AutoParts").collection("reviews");
 
+    /* ================================================= Auto Parts Start ================================================= */
     // GET API - AutoParts
     app.get("/parts", async (req, res) => {
       const result = await partsCollection.find().toArray();
@@ -54,14 +58,16 @@ async function run() {
       const result = await partsCollection.updateOne(filter, updatedPart);
       res.send(result);
     });
+    /* ================================================= Auto Parts ^ ================================================= */
 
+    /* ================================================= Orders Start ================================================= */
     // GET API - Order
-    app.get("/order", async(req, res) =>{
-      const email = req.headers.email
-      const filter = {email: email}
-      const result = await ordersCollection.find(filter).toArray()
-      res.send(result)
-    })
+    app.get("/order", async (req, res) => {
+      const email = req.headers.email;
+      const filter = { email: email };
+      const result = await ordersCollection.find(filter).toArray();
+      res.send(result);
+    });
 
     // POST API - Order
     app.post("/order", async (req, res) => {
@@ -69,6 +75,54 @@ async function run() {
       const result = await ordersCollection.insertOne(part);
       res.send(result);
     });
+    /* ================================================= Orders ^ ================================================= */
+
+    /* ================================================= Users Start ================================================= */
+    // GET API - Users
+    app.get("/user", async (req, res) => {
+      const email = req.headers.email;
+      const filter = { email: email };
+      const result = usersCollection.findOne(filter);
+      res.send(result);
+    });
+
+    // POST API - User
+    app.post("/user", async (req, res) => {
+      const userInfo = req.body;
+      const result = usersCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    // PUT API - User
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      console.log(user);
+      const options = { upsert: true };
+      const updatedUser = {
+        $set: user,
+      };
+      const result = usersCollection.updateOne(filter, updatedUser, options);
+      res.send(result);
+    });
+    /* ================================================= Users ^ ================================================= */
+
+    /* ================================================= Reviews Start ================================================= */
+    // GET API - Review
+    app.get("/review", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // POST API - Review
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      console.log(review);
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+    /* ================================================= Reviews ^ ================================================= */
   } finally {
     // Nothing Here
   }
